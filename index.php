a2ef0a459fae3aec89c16b1a7bbe1f0b44e36b9d
aniketak47 <aniketkr20012019@gmail.com>
2022-03-08 10:02:24 +0530

    first commit

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+</head>
+<body>
+	<div class="container">
+		<div class="header">
+			<img src="https://previews.123rf.com/images/ribah/ribah1501/ribah150100012/35414428-3d-illustration-of-green-right-tick-check-mark-symbol-with-word-vote.jpg" class="logo">
+			<div class="navbar">
+				<a href="#">HOME</a>
+				<a href="#">CONTACT US</a>
+			</div>
+			<button class="btn"><a href="login1.php">LOGIN</a></button>
+            <button class="btn"><a href="routes/register.php">REGISTER</a></button>
+		</div>
+		<div class="content">
+			<h1>Online Voting System</h1>
+		</div>
+	</div>
+</body>
+</html>
